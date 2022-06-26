c7731dd227864731b61210b1385154394f6b803e
daviddoret <david.doret@me.com>
2022-06-26 07:38:14 +0200

    readthedocs: troubleshooting

--- naive/hello_world.py
+++ naive/hello_world.py
@@ -1,2 +1,7 @@
-def say_hello():
-    return 'hello world'
+def say_hello(my_name: str = 'anonymous') -> str:
+    """Say hello
+
+    :param my_name: Your name
+    :return: A greeting message
+    """
+    return f'hello {my_name}'
--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -4,4 +4,5 @@ import naive.hello_world as hi
 
 class Test(TestCase):
     def test_say_hello(self):
-        self.assertEqual(hi.say_hello(), "hello world")
+        self.assertEqual(hi.say_hello(), "hello anonymous")
+        self.assertEqual(hi.say_hello('foobar'), "hello foobar")
