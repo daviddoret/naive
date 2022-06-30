f80d02f6e80e3211aff50febb3a8e98c12cfaa5c
daviddoret <david.doret@me.com>
2022-07-01 00:53:05 +0200

    jupyter-sphinx: troubleshooting local module importation

--- docs/build/jupyter_execute/pages/naive.type_library.FiniteSet.py
+++ docs/build/jupyter_execute/pages/naive.type_library.FiniteSet.py
@@ -4,12 +4,7 @@
 # In[1]:
 
 
-import sys
-import os
-#o = tl.Element('hello')
-#print(o)
-print(f'sys.path: {sys.path}')
-print(f'os.getcwd(): {os.getcwd()}')
-print('hello xxx world')
 import naive.type_library as tl
+o = tl.Element('hello')
+print(o)
 
--- naive/type_library.py
+++ naive/type_library.py
@@ -381,14 +381,9 @@ class FiniteSet(list):
 
         .. jupyter-execute::
 
-            import sys
-            import os
-            #o = tl.Element('hello')
-            #print(o)
-            print(f'sys.path: {sys.path}')
-            print(f'os.getcwd(): {os.getcwd()}')
-            print('hello xxx world')
             import naive.type_library as tl
+            o = tl.Element('hello')
+            print(o)
 
     """
 
