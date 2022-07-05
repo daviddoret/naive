d3c32f4ba0945e046cef91034964c95c16a813af
daviddoret <david.doret@me.com>
2022-07-05 23:46:22 +0200

    ci

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='naive',
+      version='{version}',
+      description="Under development",
+      long_description="",
+      author='David Doret',
+      author_email='',
+      license='MIT',
+      packages=['naive'],
+      zip_safe=False,
+      install_requires=[
+            'numpy',
+            'nptyping',
+            'typing',
+            'dataclasses'
+          ],
+      )
