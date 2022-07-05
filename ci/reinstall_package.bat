9c63292ebc9a2b10c33937a72b69b651b211fc22
daviddoret <david.doret@me.com>
2022-07-05 23:52:23 +0200

    ci

--- ci/reinstall_package.bat
+++ ci/reinstall_package.bat
@@ -1,5 +1,14 @@
+@ECHO.
+@ECHO REINSTALL PACKAGE
+@ECHO.
+REM Bibliography:
+REM - https://snarky.ca/why-you-should-use-python-m-pip/
 
-
+@ECHO PIP UNINSTALL
+@ECHO.
 python -m pip uninstall naive -y
+
+@ECHO PIP INSTALL
+@ECHO.
 REM python -m pip install naive
 python -m pip install -e .
