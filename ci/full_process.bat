3e02eec057988f7a9328d52a3bbcea547ec53f24
daviddoret <david.doret@me.com>
2022-07-05 22:20:56 +0200

    ci

--- ci/full_process.bat
+++ ci/full_process.bat
@@ -2,15 +2,16 @@
 REM @ECHO SET PATH
 REM CALL .\ci\set_syspath.bat
 
+@ECHO
 @ECHO INCREMENT PACKAGE VERSION
 CALL .\ci\increment_package_version.bat
-
+@ECHO
 @ECHO BUILD PACKAGE
 CALL .\ci\build_package.bat
-
+@ECHO
 @ECHO REINSTALL PACKAGE
 CALL .\ci\reinstall_package.bat
-
+@ECHO
 @ECHO BUILD DOCUMENTATION
 CALL .\ci\build_documentation.bat
-
+@ECHO
