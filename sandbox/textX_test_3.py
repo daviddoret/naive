bddcaf61a02936a86be15bafb0247fb31831a58c
daviddoret <david.doret@me.com>
2022-07-23 15:41:07 +0200

    textX: remove superfluous parenthesis

--- sandbox/textX_test_3.py
+++ sandbox/textX_test_3.py
@@ -3,7 +3,7 @@ from textx import metamodel_from_file
 ba1_meta = metamodel_from_file(r'..\src\naive\data\ba1_utf8.tx')
 
 
-ba1_sample_1 = r'(b1 ∧ b2)'
+ba1_sample_1 = r'(b1 ∧ (((b2))))'
 
 ba1_model = ba1_meta.model_from_str(ba1_sample_1)
 
