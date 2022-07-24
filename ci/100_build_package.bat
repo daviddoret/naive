fb4439c70a38ec3a29e5a9f5b02e9c60e7b3e752
daviddoret <david.doret@me.com>
2022-07-24 18:55:41 +0200

    DOT

--- ci/100_build_package.bat
+++ ci/100_build_package.bat
@@ -1,2 +1,6 @@
 REM https://setuptools.pypa.io/en/latest/userguide/quickstart.html
 python -m build
+
+@ECHO TODO: Copy the latest distribution \dist\naive-0.0.1.tar.gz
+@ECHO   to overwrite \dist\naive-latest.tar.gz
+
