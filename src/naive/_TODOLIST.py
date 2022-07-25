a0909b723174bce0c5fe1c0cf59a496e28f92f2b
daviddoret <david.doret@me.com>
2022-07-25 23:45:26 +0200

    Facets and ST1

--- src/naive/_TODOLIST.py
+++ src/naive/_TODOLIST.py
@@ -1,17 +1,25 @@
 # LIST
 #
-# TODO: Rename facet_key to facets and provide support with generic functions for multiple facets per concept
+# TODO: Core: Rename facet_key to facets and provide support with generic functions for multiple facets per concept
 
-# TODO: Because of support for multiple facets, concept identification can no longer rely on facets. Also,
+# TODO: Core: Because of support for multiple facets, concept identification can no longer rely on facets. Also,
 #   ...the language key is ambiguous. I suggest to remove it and rely on scope + base_key to uniquely identify concepts.
 
-# TODO: Complete the declare_finite_set function.
+# TODO: ST1: Complete the declare_finite_set function.
 
-# TODO: Implement the is_element_of binary operator. e --> S. This will require a "local" Universal set.
+# TODO: ST1: Implement the is_element_of binary operator. e --> S. This will require a "local" Universal set.
 
-# TODO: Implement the has_element binary operator.
+# TODO: ST1: Implement the has_element binary operator.
 
-# TODO: Implement the subset and strict subset operators.
+# TODO: ST1: Implement the subset and strict subset operators.
 
-# TODO: Implement the superset and strict superset operators.
+# TODO: ST1: Implement the superset and strict superset operators.
+
+# TODO: BA2: Enriched Boolean Logic algebra with all operators, etc.
+
+# TODO: NA1: Basic natural number algebra, e.g. with N0
+
+# TODO: BA1: Satisfies operator.
+
+# TODO: Move Facets to a specialized Facets class umbrella.
 
